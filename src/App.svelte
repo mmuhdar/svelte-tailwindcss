21c1eb9706fa5130a686be9d7ad75cf00c908ca5
Muhammad Muhdar <mmuhdar17@gmail.com>
2021-02-08 23:51:12 +0700

    svelte-tailwind package

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+	<div class="flex items-center justify-center min-h-screen">
+		<div class="w-1/2">
+			<div class="bg-white shadow p-6 rounded">
+				<div class="flex flex-wrap">
+					<div class="bg-teal-500 w-10 h-10 rounded">
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+<style>
+	
+</style>
